7967adce088a1894e5b003f64277da4a8b29fc77
KenEzekiel <16521040@mahasiswa.itb.ac.id>
2021-10-11 15:22:22 +0700

    update

---
+++ pathalgorithm.py
@@ -0,0 +1,36 @@
+# Algorithm
+from GUI import peta
+
+input_lokasi_awal = int(input("masukan lokasi awal: "))
+result = []
+def valid():
+    global result
+    for i in peta:
+        for j in peta[i]:
+            if peta[i][j] == input_lokasi_awal:
+                result = [i, j]
+    print(f"x = {int(i)}")
+    print(f"y = {int(i)}")
+valid()
+
+'''
+    for move in moves:
+        if move == "L":
+            i -= 1
+
+        elif move == "R":
+            i += 1
+
+        elif move == "U":
+            j -= 1
+
+        elif move == "D":
+            j += 1
+
+        if not(0 <= i < len(map[0]) and 0 <= j < len(map)):
+            return False
+        elif (maze[j][i] == "0"):
+            return False
+    
+    return true
+'''
